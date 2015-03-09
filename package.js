0b42bd8407136c399a1d5aa2836519664e39e6d4
Eric Dobbertin <eric@dairystatedesigns.com>
2015-03-09 12:00:26 -0500

    support af5

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'aldeed:autoform-bs-button-group-input',
   summary: 'A Bootstrap button group theme for the "select-checkbox" and "select-radio" AutoForm input types',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/aldeed/meteor-autoform-bs-button-group-input.git'
 });
 
 Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0-pre.1');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0');
   api.addFiles([
     'cb-buttons.html',
     'cb-buttons.js',
